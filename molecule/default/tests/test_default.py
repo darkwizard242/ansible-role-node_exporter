4cab1834a538b9afc98b14f7d82016bda06c5a42
darkwizard242 <am900820@gmail.com>
2020-09-05 00:24:44 -0400

    testinfra | fix string literal duplications

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,38 +6,45 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+NODE_EXPORTER_USER = "node_exporter"
+NODE_EXPORTER_GROUP = "node_exporter"
+NODE_EXPORTER_BINARY_FILE = "/usr/local/bin/node_exporter"
+NODE_EXPORTER_SERVICE_FILE = "/etc/systemd/system/node_exporter.service"
+NODE_EXPORTER_SERVICE_NAME = "node_exporter"
+
+
 def test_node_exporter_user_exists(host):
-    host.user('node_exporter').exists
+    host.user(NODE_EXPORTER_USER).exists
 
 
 def test_node_exporter_group_check(host):
-    host.user('node_exporter').group == 'node_exporter'
+    host.user(NODE_EXPORTER_USER).group == NODE_EXPORTER_GROUP
 
 
 def test_node_exporter_home_check(host):
-    host.user('node_exporter').home == '/bin/false'
+    host.user(NODE_EXPORTER_USER).home == '/bin/false'
 
 
 def test_node_exporter_binary_exists(host):
-    host.file('/usr/local/bin/node_exporter').exists
+    host.file(NODE_EXPORTER_BINARY_FILE).exists
 
 
 def test_node_exporter_binary_isfile(host):
-    assert host.file('/usr/local/bin/node_exporter').is_file
+    assert host.file(NODE_EXPORTER_BINARY_FILE).is_file
 
 
 def test_node_exporter_binary_which(host):
     assert host.check_output('which node_exporter') == \
-      '/usr/local/bin/node_exporter'
+      NODE_EXPORTER_BINARY_FILE
 
 
 def test_node_exporter_service_file_exists(host):
-    host.file('/etc/systemd/system/node_exporter.service').exists
+    host.file(NODE_EXPORTER_SERVICE_FILE).exists
 
 
 def test_node_exporter_service_is_enabled(host):
-    assert host.service('node_exporter').is_enabled
+    assert host.service(NODE_EXPORTER_SERVICE_NAME).is_enabled
 
 
 def test_node_exporter_service_is_running(host):
-    assert host.service('node_exporter').is_running
+    assert host.service(NODE_EXPORTER_SERVICE_NAME).is_running
