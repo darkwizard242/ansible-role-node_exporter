b23890044e093492731a57cf1128d02387972f44
darkwizard242 <am900820@gmail.com>
2020-09-05 01:11:45 -0400

    testinfra | refactor functions for node_exporter

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,46 +6,42 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-NODE_EXPORTER_USER = "node_exporter"
-NODE_EXPORTER_GROUP = "node_exporter"
-NODE_EXPORTER_HOME = "/bin/false"
-NODE_EXPORTER_BINARY_FILE = "/usr/local/bin/node_exporter"
-NODE_EXPORTER_SERVICE_FILE = "/etc/systemd/system/node_exporter.service"
-NODE_EXPORTER_SERVICE_NAME = "node_exporter"
+NE_USER = "node_exporter"
+NE_BINARY_FILE = "/usr/local/bin/node_exporter"
+NE_SERVICE_FILE = "/etc/systemd/system/node_exporter.service"
+NE_SERVICE_NAME = "node_exporter"
 
 
 def test_node_exporter_user_exists(host):
-    host.user(NODE_EXPORTER_USER).exists
-
-
-def test_node_exporter_group_check(host):
-    host.user(NODE_EXPORTER_USER).group == 'node_exporter'
-
-
-def test_node_exporter_home_check(host):
-    host.user(NODE_EXPORTER_USER).home == '/bin/false'
+    """Tests if node_exporter user exists."""
+    host.user(NE_USER).exists
 
 
 def test_node_exporter_binary_exists(host):
-    host.file(NODE_EXPORTER_BINARY_FILE).exists
+    """Tests if /usr/local/bin/node_exporter file exists."""
+    host.file(NE_BINARY_FILE).exists
 
 
 def test_node_exporter_binary_isfile(host):
-    assert host.file(NODE_EXPORTER_BINARY_FILE).is_file
+    """Tests if /usr/local/bin/node_exporter file type is file."""
+    assert host.file(NE_BINARY_FILE).is_file
 
 
 def test_node_exporter_binary_which(host):
-    assert host.check_output('which node_exporter') == \
-      NODE_EXPORTER_BINARY_FILE
+    """Tests output of command matches /usr/local/bin/node_exporter"""
+    assert host.check_output('which node_exporter') == NE_BINARY_FILE
 
 
 def test_node_exporter_service_file_exists(host):
-    host.file(NODE_EXPORTER_SERVICE_FILE).exists
+    """Tests if /etc/systemd/system/node_exporter.service file exists."""
+    host.file(NE_SERVICE_FILE).exists
 
 
 def test_node_exporter_service_is_enabled(host):
-    assert host.service(NODE_EXPORTER_SERVICE_NAME).is_enabled
+    """Tests if node_exporter.service is enabled."""
+    assert host.service(NE_SERVICE_NAME).is_enabled
 
 
 def test_node_exporter_service_is_running(host):
-    assert host.service(NODE_EXPORTER_SERVICE_NAME).is_running
+    """Tests if node_exporter.service is running."""
+    assert host.service(NE_SERVICE_NAME).is_running
